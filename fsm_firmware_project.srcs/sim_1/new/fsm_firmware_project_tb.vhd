f2cb18415ac16a044575eef96a36028ec9d0e41b
Anders Barzdukas abarzdukas@umail.ucsb.edu <abarzdukas@higgs.physics.ucsb.edu>
2024-07-16 22:58:43 -0700

    Adding testbench and a bit more.

---
+++ fsm_firmware_project.srcs/sim_1/new/fsm_firmware_project_tb.vhd
@@ -0,0 +1,60 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date: 07/16/2024 10:44:19 PM
+-- Design Name: 
+-- Module Name: fsm_firmware_project_tb - Behavioral
+-- Project Name: 
+-- Target Devices: 
+-- Tool Versions: 
+-- Description: 
+-- 
+-- Dependencies: 
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+-- 
+----------------------------------------------------------------------------------
+
+
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--use IEEE.NUMERIC_STD.ALL;
+
+-- Uncomment the following library declaration if instantiating
+-- any Xilinx leaf cells in this code.
+--library UNISIM;
+--use UNISIM.VComponents.all;
+
+
+--This is an extremely basic top file that creates a clock and sends it to the fsm_firmware_project_top file.
+entity fsm_firmware_project_tb is
+end fsm_firmware_project_tb;
+
+architecture Behavioral of fsm_firmware_project_tb is
+
+--Defining a clk signal and giving it an initial value
+signal clk : std_logic := '0';
+
+--Declaring top file as a component
+component fsm_firmware_project_top
+port(clk : std_logic);
+end component;
+
+begin
+
+--Creates a clock that alternates every 20 ns
+clk <= not clk after 100 ns;
+
+--Instatiates the top file in this simulation file
+u_fsm_firmware_project_top : fsm_firmware_project_top port map(clk => clk);
+
+
+
+
+end Behavioral;
